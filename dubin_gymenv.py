18458c27da8b1e98724b4af2671d58843cb33992
Gargi Vaidya <64918010+gargivaidya@users.noreply.github.com>
2021-04-15 00:18:41 -0500

    Update dubin_gymenv.py

--- dubin_gymenv.py
+++ dubin_gymenv.py
@@ -5,9 +5,56 @@ import gym
 from gym import spaces
 import time
 import itertools
+import argparse
+import datetime
 
 from stable_baselines.sac.policies import MlpPolicy
 from stable_baselines import SAC
+import torch
+from sac import SAC
+from replay_memory import ReplayMemory
+from torch.utils.tensorboard import SummaryWriter
+
+parser = argparse.ArgumentParser(description='PyTorch Soft Actor-Critic Args')
+parser.add_argument('--env-name', default="HalfCheetah-v2",
+                    help='Mujoco Gym environment (default: HalfCheetah-v2)')
+parser.add_argument('--policy', default="Gaussian",
+                    help='Policy Type: Gaussian | Deterministic (default: Gaussian)')
+parser.add_argument('--eval', type=bool, default=True,
+                    help='Evaluates a policy a policy every 10 episode (default: True)')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+                    help='discount factor for reward (default: 0.99)')
+parser.add_argument('--tau', type=float, default=0.005, metavar='G',
+                    help='target smoothing coefficient(τ) (default: 0.005)')
+parser.add_argument('--lr', type=float, default=0.0003, metavar='G',
+                    help='learning rate (default: 0.0003)')
+parser.add_argument('--alpha', type=float, default=0.2, metavar='G',
+                    help='Temperature parameter α determines the relative importance of the entropy\
+                            term against the reward (default: 0.2)')
+parser.add_argument('--automatic_entropy_tuning', type=bool, default=False, metavar='G',
+                    help='Automaically adjust α (default: False)')
+parser.add_argument('--seed', type=int, default=123456, metavar='N',
+                    help='random seed (default: 123456)')
+parser.add_argument('--batch_size', type=int, default=256, metavar='N',
+                    help='batch size (default: 256)')
+parser.add_argument('--num_steps', type=int, default=50000, metavar='N',
+                    help='maximum number of steps (default: 1000000)')
+parser.add_argument('--hidden_size', type=int, default=256, metavar='N',
+                    help='hidden size (default: 256)')
+parser.add_argument('--updates_per_step', type=int, default=1, metavar='N',
+                    help='model updates per simulator step (default: 1)')
+parser.add_argument('--start_steps', type=int, default=1000, metavar='N',
+                    help='Steps sampling random actions (default: 10000)')
+parser.add_argument('--target_update_interval', type=int, default=1, metavar='N',
+                    help='Value target update per no. of updates per step (default: 1)')
+parser.add_argument('--replay_size', type=int, default=1000000, metavar='N',
+                    help='size of replay buffer (default: 10000000)')
+parser.add_argument('--cuda', action="store_true",
+                    help='run on CUDA (default: False)')
+parser.add_argument('--max_episode_length', type=int, default=300, metavar='N',
+					help='max episode length (default: 3000)')
+args = parser.parse_args()
+
 
 MAX_STEER = np.pi/3
 MAX_SPEED = 10.0
@@ -61,8 +108,10 @@ class DubinGym(gym.Env):
 		y = self.pose[1]
 		yaw_car = self.pose[2]
 		head = math.atan((y_target-y)/(x_target-x+0.01))
+		print(self.get_distance([0,0,1.57], [0,5, 1.57]))
+		print(self.get_heading([0,0,1.57], [0,5, 1.57]))
 		return -1*(abs(x - x_target) + abs(y - y_target) + abs (head - yaw_car))
-	
+
 	def get_distance(self,x1,x2):
 		return math.sqrt((x1[0] - x2[0])**2 + (x1[1] - x2[1])**2)
 
@@ -74,8 +123,8 @@ class DubinGym(gym.Env):
 		cross_track_err 
 		return reward
 
-	def next_waypoint(self):
-		d_to_waypoints = np.zeros(self.n_waypoints):
+	def next_index(self):
+		d_to_waypoints = np.zeros(self.n_waypoints)
 
 		for i in range(self.n_waypoints):
 			d_to_waypoints[i] = self.get_distance(self.waypoints[i], self.pose)
@@ -89,7 +138,7 @@ class DubinGym(gym.Env):
 		info = {}
 		self.action = action
 		self.pose = self.update_state(self.pose, action, 0.005)
-		print(self.pose)
+		# print(self.pose)
 
 		if ((abs(self.pose[0]) < 1.) and (abs(self.pose[1]) < 1.)):
 
@@ -97,12 +146,14 @@ class DubinGym(gym.Env):
 				reward = 10            
 				done = True
 				print('Goal Reached')
+				print("Distance : ", abs(self.pose[0]-self.target[0]), abs(self.pose[1]-self.target[1]))
 			else:
 				reward = self.get_reward()	
 		else :
 			done = True
 			reward = -1.
 			print("Outside range")
+			print("Distance : ", abs(self.pose[0]-self.target[0]), abs(self.pose[1]-self.target[1]))
 
 		return np.array(self.pose), reward, done, info     
 
@@ -214,22 +265,103 @@ def main():
 
 	start_point = [0., 0., 1.57]
 	target_point = [0., 5., 1.57]
-	env =  DubinGym(start_point, target_point)
+	waypoints = [[0., 1., 1.57],[0., 2.5, 1.57],[0., 4., 1.57]]
+	n_waypoints = 3
+	env =  DubinGym(start_point, waypoints, target_point, n_waypoints)
 	max_steps = int(1e6)
 
-	state = env.reset()
-	env.render()
-	for ep in range(5):
+	## Model Training
+
+	agent = SAC(env.observation_space.shape[0], env.action_space, args)
+	# Memory
+	memory = ReplayMemory(args.replay_size, args.seed)
+	#Tesnorboard
+	writer = SummaryWriter('runs/{}_SAC_{}_{}_{}'.format(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S"), 'DeepracerGym',
+														 args.policy, "autotune" if args.automatic_entropy_tuning else ""))
+
+	total_numsteps = 0
+	updates = 0
+	num_goal_reached = 0
+
+	for i_episode in itertools.count(1):
+		# print("New episode")
+		episode_reward = 0
+		episode_steps = 0
+		done = False
 		state = env.reset()
-		env.render()
-		for i in range(max_steps):
-			action = [1.0, 0.]
-			n_state,reward,done,info = env.step(action)
+		
+		while not done:
 			env.render()
-			if done:
-				state = env.reset()
-				done = False                   
-				break
+			start_time = time.time()
+			if args.start_steps > total_numsteps:
+				action = env.action_space.sample()  # Sample random action
+			else:
+				action = agent.select_action(state)  # Sample action from policy
+
+			next_state, reward, done, _ = env.step(action) # Step
+			if (reward > 9) and (episode_steps > 1): #Count the number of times the goal is reached
+				num_goal_reached += 1 
+
+			episode_steps += 1
+			total_numsteps += 1
+			episode_reward += reward
+			if episode_steps > args.max_episode_length:
+				done = True
+
+			# Ignore the "done" signal if it comes from hitting the time horizon.
+			# (https://github.com/openai/spinningup/blob/master/spinup/algos/sac/sac.py)
+			mask = 1 if episode_steps == args.max_episode_length else float(not done)
+			# mask = float(not done)
+			memory.push(state, action, reward, next_state, mask) # Append transition to memory
+
+			state = next_state
+			# print(done)
+
+		# if i_episode % UPDATE_EVERY == 0: 
+		if len(memory) > args.batch_size:
+			# Number of updates per step in environment
+			for i in range(args.updates_per_step*args.max_episode_length):
+				# Update parameters of all the networks
+				critic_1_loss, critic_2_loss, policy_loss, ent_loss, alpha = agent.update_parameters(memory, args.batch_size, updates)
+
+				writer.add_scalar('loss/critic_1', critic_1_loss, updates)
+				writer.add_scalar('loss/critic_2', critic_2_loss, updates)
+				writer.add_scalar('loss/policy', policy_loss, updates)
+				writer.add_scalar('loss/entropy_loss', ent_loss, updates)
+				writer.add_scalar('entropy_temprature/alpha', alpha, updates)
+				updates += 1
+
+		if total_numsteps > args.num_steps:
+			break
+
+		if (episode_steps > 1):
+			writer.add_scalar('reward/train', episode_reward, i_episode)
+			writer.add_scalar('reward/episode_length',episode_steps, i_episode)
+			writer.add_scalar('reward/num_goal_reached',num_goal_reached, i_episode)
+
+		print("Episode: {}, total numsteps: {}, episode steps: {}, reward: {}".format(i_episode, total_numsteps, episode_steps, round(episode_reward, 2)))
+		print("Number of Goals Reached: ",num_goal_reached)
+
+	print('----------------------Training Ending----------------------')
+	# env.stop_car()
+
+	agent.save_model("dubin_straight", suffix = "1")
+	return True
+
+	## Environment Test
+	# state = env.reset()
+	# env.render()
+	# for ep in range(5):
+	# 	state = env.reset()
+	# 	env.render()
+	# 	for i in range(max_steps):
+	# 		action = [1.0, 0.]
+	# 		n_state,reward,done,info = env.step(action)
+	# 		env.render()
+	# 		if done:
+	# 			state = env.reset()
+	# 			done = False                   
+	# 			break
 
 if __name__ == '__main__':
 	main()

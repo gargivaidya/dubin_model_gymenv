821a112fb7b84e06902fe7f7a180f4705e4e85ff
jain.anmollkumar <anmollkumarjain@Anmolls-MacBook-Pro.local>
2021-04-26 01:38:01 -0500

    test script for chaining

--- dubins_gym_evaluation.py
+++ dubins_gym_evaluation.py
@@ -53,14 +53,120 @@ parser.add_argument('--max_episode_length', type=int, default=300, metavar='N',
 					help='max episode length (default: 3000)')
 args = parser.parse_args()
 
+
+# Training constants
+MAX_STEER = np.pi/3
+MAX_SPEED = 10.0
+MIN_SPEED = 0.
+THRESHOLD_DISTANCE_2_GOAL = 0.02
 MAX_X = 5.
 MAX_Y = 5.
+THETA0 = np.pi/4
+
+# Vehicle parameters
+LENGTH = 0.45  # [m]
+WIDTH = 0.2  # [m]
+BACKTOWHEEL = 0.1  # [m]
+WHEEL_LEN = 0.03  # [m]
+WHEEL_WIDTH = 0.02  # [m]
+TREAD = 0.07  # [m]
+WB = 0.25  # [m]
+
+
+def plot_car(pose, action, cabcolor="-r", truckcolor="-k"):  # pragma: no cover
+		# print("Plotting Car")
+		# Scale up the car pose to MAX_X, MAX_Y grid
+		x = pose[0] 
+		y = pose[1] 
+		yaw = pose[2]
+		steer = action[1]*MAX_STEER 
+
+		outline = np.array([[-BACKTOWHEEL, (LENGTH - BACKTOWHEEL), (LENGTH - BACKTOWHEEL), -BACKTOWHEEL, -BACKTOWHEEL],
+							[WIDTH / 2, WIDTH / 2, - WIDTH / 2, -WIDTH / 2, WIDTH / 2]])
+
+		fr_wheel = np.array([[WHEEL_LEN, -WHEEL_LEN, -WHEEL_LEN, WHEEL_LEN, WHEEL_LEN],
+							 [-WHEEL_WIDTH - TREAD, -WHEEL_WIDTH - TREAD, WHEEL_WIDTH - TREAD, WHEEL_WIDTH - TREAD, -WHEEL_WIDTH - TREAD]])
+
+		rr_wheel = np.copy(fr_wheel)
+
+		fl_wheel = np.copy(fr_wheel)
+		fl_wheel[1, :] *= -1
+		rl_wheel = np.copy(rr_wheel)
+		rl_wheel[1, :] *= -1
+
+		Rot1 = np.array([[math.cos(yaw), math.sin(yaw)],
+						 [-math.sin(yaw), math.cos(yaw)]])
+		Rot2 = np.array([[math.cos(steer), math.sin(steer)],
+						 [-math.sin(steer), math.cos(steer)]])
+
+		fr_wheel = (fr_wheel.T.dot(Rot2)).T
+		fl_wheel = (fl_wheel.T.dot(Rot2)).T
+		fr_wheel[0, :] += WB
+		fl_wheel[0, :] += WB
+
+		fr_wheel = (fr_wheel.T.dot(Rot1)).T
+		fl_wheel = (fl_wheel.T.dot(Rot1)).T
+
+		outline = (outline.T.dot(Rot1)).T
+		rr_wheel = (rr_wheel.T.dot(Rot1)).T
+		rl_wheel = (rl_wheel.T.dot(Rot1)).T
+
+		outline[0, :] += x
+		outline[1, :] += y
+		fr_wheel[0, :] += x
+		fr_wheel[1, :] += y
+		rr_wheel[0, :] += x
+		rr_wheel[1, :] += y
+		fl_wheel[0, :] += x
+		fl_wheel[1, :] += y
+		rl_wheel[0, :] += x
+		rl_wheel[1, :] += y
+
+		plt.plot(np.array(outline[0, :]).flatten(),
+				 np.array(outline[1, :]).flatten(), truckcolor)
+		plt.plot(np.array(fr_wheel[0, :]).flatten(),
+				 np.array(fr_wheel[1, :]).flatten(), truckcolor)
+		plt.plot(np.array(rr_wheel[0, :]).flatten(),
+				 np.array(rr_wheel[1, :]).flatten(), truckcolor)
+		plt.plot(np.array(fl_wheel[0, :]).flatten(),
+				 np.array(fl_wheel[1, :]).flatten(), truckcolor)
+		plt.plot(np.array(rl_wheel[0, :]).flatten(),
+				 np.array(rl_wheel[1, :]).flatten(), truckcolor)
+		plt.plot(x, y, "*")
+
+
+def render(pose, action, traj, waypoints, reset=False):
+
+	if reset:
+		traj[0].clear()
+		traj[1].clear()
+	
+	# Storing tracked trajectory 
+	traj[0].append(pose[0])
+	traj[1].append(pose[1])
+	
+	plt.cla()
+	# for stopping simulation with the esc key.
+	plt.gcf().canvas.mpl_connect('key_release_event',
+			lambda event: [exit(0) if event.key == 'escape' else None])
+	plt.plot(traj[0], traj[1], "ob", markersize = 2, label="trajectory")
+	# Rendering waypoint sequence
+	for i in range(len(waypoints)-1):
+		plt.plot(waypoints[i][0], waypoints[i][1], "^r", label="waypoint")
+	plt.plot(waypoints[-1][0], waypoints[-1][1], "xg", label="target")
+	# Rendering the car and action taken
+	plot_car(pose, action)
+	plt.axis("equal")
+	plt.grid(True)
+	plt.title("Simulation")
+	plt.pause(0.0001)
+
 
 def pose_transform(pose, target, subtract):
-	if subtract:
-		return pose[0]-target[0], pose[1]-target[1]
+	if not subtract:
+		return (pose[0]*MAX_X)+target[0], (pose[1]*MAX_Y)+target[1]
 	else:
-		return pose[0]+target[0], pose[1]+target[1]
+		return (pose[0]-target[0])/MAX_X, (pose[1]-target[1])/MAX_X
 
 def get_distance(x1,x2):
 	return math.sqrt((x1[0] - x2[0])**2 + (x1[1] - x2[1])**2)
@@ -83,6 +189,7 @@ def main():
 	# target_point = [4., 8., 1.57]
 	waypoints = [[0., 1., 1.57], [0., 2., 1.57],[0.5, 3., 1.57], [1., 4., 1.57], [2., 5., 1.57], [3., 6., 1.57], [3., 7., 1.57], [4., 8., 1.57]]
 	#waypoints = [[-1., 0., 1.57]]
+	traj = [[],[]]
 
 	env =  DubinGym(start_point)
 
@@ -109,6 +216,9 @@ def main():
 		goals_in_iter = 0
 		current_pose = [x for x in start_point]
 
+		#Render environment
+		render(current_pose, [0., 0.], traj, waypoints, True)
+
 		for i_waypoint in range(len(waypoints)):
 
 			ep_reward = 0.
@@ -116,7 +226,7 @@ def main():
 
 			# Transform pose to real world
 			if i_waypoint > 0:
-				current_pose[0], current_pose[1] = pose_transform([state[0]*MAX_X, state[1]*MAX_Y], target, False)
+				current_pose[0], current_pose[1] = pose_transform([state[0], state[1]], target, False)
 				current_pose[2] = state[2]
 
 			#Update target point
@@ -124,13 +234,11 @@ def main():
 
 			#Transform pose to model architecture
 			x, y = pose_transform(current_pose, target, True)
-			state = env.pose = [x/MAX_X, y/MAX_Y, current_pose[2]]
+			state = env.pose = [x, y, current_pose[2]]
 
 			for _ in range(max_steps):
 				action = agent.select_action(state)
 				next_state, reward, done, _ = env.step(action)
-
-				env.render()
 				ep_reward += reward
 
 				#print("\r Car is at : {}, reward : {:.4f}".format(next_state, reward), end = '\r')
@@ -143,10 +251,16 @@ def main():
 				# 	if get_distance([x,y], waypoints[i_waypoint+1]) < (get_distance([x,y], target) + get_distance(target, waypoints[i_waypoint+1])):
 				# 		done = True
 
+				# Transform pose to real world
+				current_pose[0], current_pose[1] = pose_transform(state, target, False)
+				current_pose[2] = state[2]
+				
+				#Render environment
+				render(current_pose, action, traj, waypoints)
+
 				# Check if target needs to be updated
 				if i_waypoint > 0:
-					x, y = pose_transform(state, target, False)
-					if get_closest_idx(waypoints, [x, y]) > i_waypoint:
+					if get_closest_idx(waypoints, current_pose[:2]) > i_waypoint:
 						done = True
 
 				# if done with current goal - reached or passed
